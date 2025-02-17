55895f13720fc104e2f45f04f3d58a2e2814d7d7
sambelltacq <sam.bell@d-tacq.co.uk>
2025-02-17 11:32:00 +0000

    Setup dirs 	deleted:    index.html 	new file:   run_phoebus.sh 	new file:   src/images/image.png 	new file:   src/launcher_here.txt 	new file:   src/opi/component/widget1.bob 	new file:   src/opi/opi1.bob 	new file:   src/scripts/script1.py 	deleted:    test1.bob

---
+++ run_phoebus.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+#require UUT arg
+#check if existing workspsace
+#run layout file for existing workpsace
+#else run base resource file and create worksapce dir
+#change phoebus home location to workspace dir
+
+#Serve from http?
+
+
