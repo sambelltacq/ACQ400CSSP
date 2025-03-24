b8b2ec0b3318deb08df7f88b29aacbe675a63d49
sambelltacq <Sam.Bell@d-tacq.co.uk>
2025-03-24 14:41:45 +0000

    Added support for mulitple version of Java 	modified:   run_phoebus.bat

--- run_phoebus.bat
+++ run_phoebus.bat
@@ -23,6 +23,7 @@ set "PHOEBUS_JAR=%ROOT_DIR%\phoebus-4.7.4-SNAPSHOT\product-4.7.4-SNAPSHOT.jar"
 
 :: Vars
 set "JAVA_EXE=java"
+::set "JAVA_EXE=C:\Program Files\Eclipse Adoptium\jre-17.0.12.7-hotspot\bin\java.exe" & :: Set full java path if multiple version installed
 set "SETTINGS_BASE=%ROOT_DIR%\src\settings_base.ini"
 set "LAUNCHER=%ROOT_DIR%\src\acq400_launcher.bob"
 set "RESOURCE=%LAUNCHER%"
@@ -77,7 +78,7 @@ if not exist "%MEMENTO%" (
 )
 
 :: Run
-set CMD=%JAVA_EXE% %JAVA_PREFS% -jar "%PHOEBUS_JAR%" -nosplash -settings "%SETTINGS%" %TARGET%
+set CMD="%JAVA_EXE%" %JAVA_PREFS% -jar "%PHOEBUS_JAR%" -nosplash -settings "%SETTINGS%" %TARGET%
 echo Running cmd: %CMD%
 %CMD% >nul 2>&1
 echo "Done"
