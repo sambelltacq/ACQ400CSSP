cecfcdf218687d7d58a096154590cb0db7b18d72
sambelltacq <Sam.Bell@d-tacq.co.uk>
2025-08-14 15:41:25 +0100

    Removed tooltip requirement from action button script 	modified:   src/scripts/handle_action_button.py

--- src/scripts/handle_action_button.py
+++ src/scripts/handle_action_button.py
@@ -14,13 +14,14 @@ def get_macros(widget):
     macros = widget.getEffectiveMacros()
     return {key: macros.getValue(key) for key in macros.names}
 
-def get_target(widget, propname="tooltip"):
-    prop = widget.getPropertyValue(propname).split(' ')
-    if len(prop) > 2:
-        prop[2] = dict(kv.split('=') for kv in prop[2].split('/'))
+def get_target(widget):
+    action = str(widget.propActions()).split(' ')
+
+    if len(action) > 2:
+        action[2] = dict(kv.split('=') for kv in action[2].split('/'))
     else:
-        prop.append({})
-    return prop[0], prop[1], prop[2]
+        action.append({})
+    return action[0], action[1], action[2]
 
 macros = {}
 widget_macros = get_macros(widget)
@@ -34,6 +35,7 @@ if global_widget:
 # Combine macros from display, widget and button
 cmd, target, button_macros = get_target(widget)
 macros.update(button_macros)
+
 #macros['timestamp'] = str(int(time.time())) # remove?
 
 logger.info('Pressed button {} {}'.format(cmd, target))
