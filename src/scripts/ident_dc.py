d3ea044f6acf3648d8fd16098f903dcd1198aa81
sambelltacq <sam.bell@d-tacq.co.uk>
2025-11-04 14:45:54 +0000

    Fixed error with IDENT button 	modified:   src/scripts/ident_dc.py

--- src/scripts/ident_dc.py
+++ src/scripts/ident_dc.py
@@ -18,9 +18,26 @@ def get_macros(widget):
 nchan = PVUtil.getLong(ScriptUtil.getPrimaryPV(widget))
 macros = get_macros(display)
 
-amount = 0
-pvname = "{UUT}:{SITE}:AO:SLOW_SET:CH:{chan}"
-for chan in range(1, nchan + 1):
+
+pvfmts = [
+    "{UUT}:{SITE}:AO:SLOW_SET:CH:{chan}",
+    "{UUT}:{SITE}:AO:SLOW_SET:CH:{chan:02d}"
+]
+amount = 0.125
+macros['chan'] = 1
+try:
+    pvfmt = pvfmts[0]
+    pvname = pvfmt.format(**macros)
+    PVUtil.writePV(pvname, amount, timeout)
+except:
+    pvfmt = pvfmts[1]
+    pvname = pvfmt.format(**macros)
+    PVUtil.writePV(pvname, amount, timeout)
+print("{} set to {}".format(pvname, amount))
+
+for chan in range(2, nchan + 1):
     amount += 0.125
     macros['chan'] = chan
-    PVUtil.writePV(pvname.format(**macros), amount, timeout)
+    pvname = pvfmt.format(**macros)
+    print("{} set to {}".format(pvname, amount))
+    PVUtil.writePV(pvname, amount, timeout)
